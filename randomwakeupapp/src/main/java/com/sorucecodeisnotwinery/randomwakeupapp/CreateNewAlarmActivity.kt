d03274bccb1caaeaba629071c7f6b458e53e5a10
Ivan Hušnjak <ivan@artkod.com>
2019-01-03 12:26:00 +0100

    Added time and dropdown select controls

--- randomwakeupapp/src/main/java/com/sorucecodeisnotwinery/randomwakeupapp/CreateNewAlarmActivity.kt
+++ randomwakeupapp/src/main/java/com/sorucecodeisnotwinery/randomwakeupapp/CreateNewAlarmActivity.kt
@@ -5,12 +5,27 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+
 
 class CreateNewAlarmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_alarm)
+
+        //get the spinner from the xml.
+        val dropdown = findViewById<Spinner>(R.id.spanSpinner)
+        //create a list of items for the spinner.
+        val items = arrayOf("exact", "± 2min", "± 5min", "± 10min")
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        //set the spinners adapter to the previously created one.
+        dropdown.adapter = adapter
+        dropdown.setSelection(0)
     }
 
     fun createAlarm(view: View) {
